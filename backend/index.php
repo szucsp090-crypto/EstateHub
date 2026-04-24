58d51b9a20fcfae31e4458dc2588d9920046c4d6
zsinzsini <t.fannifru@gmail.com>
2026-04-24 17:05:31 +0200

    Add index.php for backend HTML structure

---
+++ backend/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    require "config.php";
+
+
+?>
+<!DOCTYPE html>
+<html lang="hu">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>EstateHub</title>
+ </head>
+ <body>
+   <?php require "nav.php"; ?>
+ </body>
+</html>
